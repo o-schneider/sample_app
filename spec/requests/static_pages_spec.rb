cfa17e545208cf72ba55f2b70a6f6715efbc0049
Olivier Schneider <oliwan3dfx@gmail.com>
2012-02-12 18:50:07 +0100

    Add click test

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -40,4 +40,18 @@ describe "Static pages" do
 
     it_should_behave_like "all static pages"
   end
+
+  it "should have the right links on the layout" do
+    visit root_path
+    click_link "About"
+    page.should have_selector 'title', text: full_title('About Us')
+    click_link "Help"
+    page.should # fill in
+    click_link "Contact"
+    page.should # fill in
+    click_link "Home"
+    page.should # fill in
+    click_link "Sign up now!"
+    page.should # fill in
+  end
 end
