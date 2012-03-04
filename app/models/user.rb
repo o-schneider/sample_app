194aebc9650b4da5ec07ef4e8d711026c41c4a6c
Olivier SCHNEIDER <oliwan3dfx@gmail.com>
2012-03-04 21:11:17 +0100

    Implement authentication page

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,6 +13,7 @@
 class User < ActiveRecord::Base
   attr_accessible :name, :email, :password, :password_confirmation
   has_secure_password
+  before_save :create_remember_token
 
   validates :name,
             presence: true,
@@ -26,4 +27,10 @@ class User < ActiveRecord::Base
 
   validates :password,
             length: {minimum: 6}
+
+  private
+
+    def create_remember_token
+      self.remember_token = SecureRandom.urlsafe_base64
+    end
 end
