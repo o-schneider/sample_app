600685ae5617aefa24eae21add5f269e69725be3
Olivier SCHNEIDER <oliwan3dfx@gmail.com>
2012-03-05 21:48:21 +0100

    Implement form_tag for authentication

--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -14,7 +14,7 @@ RSpec::Matchers.define :have_error_message do |message|
 end
 
 def valid_signin(user)
-  fill_in "Email",    with: user.email
+  fill_in "Email", with: user.email
   fill_in "Password", with: user.password
   click_button "Sign in"
 end
