9b3986bf131e3eab9dee6b71858c718d469e2358
Olivier SCHNEIDER <oliwan3dfx@gmail.com>
2012-03-09 15:12:24 +0100

    Clean the test, part 2

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -31,10 +31,7 @@ describe "User pages" do
 
     describe "with valid information" do
       before do
-        fill_in "Name", with: "Example User"
-        fill_in "Email", with: "user@example.com"
-        fill_in "Password", with: "foobar"
-        fill_in "Confirmation", with: "foobar"
+        create_valid_user
       end
 
       it "should create a user" do
--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -7,50 +7,16 @@ def full_title(page_title)
   end
 end
 
-RSpec::Matchers.define :have_error_message do |message|
-  match do |page|
-    page.should have_selector('div.flash.error', text: message)
-  end
-end
-
-RSpec::Matchers.define :have_success_message do |message|
-  match do |page|
-    page.should have_selector('div.flash.success', text: message)
-  end
-end
-
-RSpec::Matchers.define :have_title do |text|
-  match do |page|
-    page.should have_selector('title', text: text)
-  end
-end
-
-RSpec::Matchers.define :have_profile_link do |user|
-  match do |page|
-    page.should have_link('Profile', href: user_path(user))
-  end
-end
-
-RSpec::Matchers.define :have_signout_link do
-  match do |page|
-    page.should  have_link('Sign out', href: signout_path)
-  end
-end
-
-RSpec::Matchers.define :have_signin_link do
-  match do |page|
-    page.should  have_link('Sign in', href: signin_path)
-  end
-end
-
-RSpec::Matchers.define :have_heading do |heading|
-  match do |page|
-    page.should have_selector('h1', text: heading)
-  end
-end
-
 def valid_signin(user)
   fill_in "Email", with: user.email
   fill_in "Password", with: user.password
   click_button "Sign in"
 end
+
+def create_valid_user
+  fill_in "Name", with: "Example User"
+  fill_in "Email", with: "user@example.com"
+  fill_in "Password", with: "foobar"
+  fill_in "Confirmation", with: "foobar"
+end
+
