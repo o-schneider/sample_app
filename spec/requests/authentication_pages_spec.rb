b2b07e92515279ab2e0cc1e7d970c4ab4ff5376d
Olivier SCHNEIDER <oliwan3dfx@gmail.com>
2012-03-10 20:54:27 +0100

    Add test to verify that signin twice redirect to the profile page

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -108,6 +108,19 @@ describe "Authentication" do
           it "should render the desired protected page" do
             page.should have_title('Edit user')
           end
+
+          describe "when signing in again" do
+            before do
+              visit signin_path(user)
+              fill_in "Email", with: user.email
+              fill_in "Password", with: user.password
+              click_button "Sign in"
+            end
+
+            it "should render the profile page" do
+              page.should have_title(user.name)
+            end
+          end
         end
       end
     end
